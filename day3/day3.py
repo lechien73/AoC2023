15b979fe570d0f1e007112c67e259041d642082b
Matt Rudge <matt.rudge@codeinstitute.net>
2023-12-04 09:02:29 +0000

    Days 3 and 4

--- day3/day3.py
+++ day3/day3.py
@@ -8,16 +8,21 @@ symbols = {(r, c) for r in range(len(data[0])) for c in range(len(data[0])) if d
 
 parts = defaultdict(list)
 
+# Create a set containing all of the numbers and their coordinates
+
 for i, line in enumerate(data):
     for occ in finditer(r"\d+", line):
-        neighbours = {(i+s, c+d) for s in (-1, 0, 1) 
-                            for d in (-1, 0, 1)
-                            for c in range(*occ.span())}
+        neighbours = {(i+s, c+d) for s in (-1, 0, 1) for d in (-1, 0, 1) for c in range(*occ.span())}
 
+        # Iterate through the intersected sets and create the parts
+        # dictionary
         for s in neighbours & symbols:
             parts[s] = int(occ[0])
-print(parts)
-part1 = sum(sum(part)  for part in parts.values())
+
+# Now, it's a simple case of summing each neighbouring part for part 1
+part1 = sum(sum(part) for part in parts.values())
+
+# Or getting the sum of the product of the gear ratios for part 2
 part2 = sum(part[0] * part[1] for part in parts.values() if len(part)==2)
 
 print(f"Part 1: {part1}")
---
+++ day4/day4.py
@@ -0,0 +1,38 @@
+data = open("input.txt").read().splitlines()
+part1 = 0
+part2 = 0
+
+cards_list = []
+cards = {}
+
+for i, line in enumerate(data):
+    # Populate our cards dictionary for part 2
+    cards[i + 1] = 1
+
+    line = line.split(": ")[1].split(" | ")
+
+    # Create two sets - one for winning numbers and the other for
+    # the numbers we have
+    winning_nums = {int(i.strip()) for i in line[0].split(" ") if i}
+    my_nums = {int(i.strip()) for i in line[1].split(" ") if i}
+
+    # Creates a set based on the intersection of the two sets
+    # The intersection is where numbers are in both sets.
+    intersections = winning_nums & my_nums 
+    
+    # The points are powers of 2 - we love those in programming
+    part1 += int(2 ** (len(intersections) - 1))
+
+    cards_list.append(len(intersections))
+
+# Lots of ugly for loops in here
+for i, card in enumerate(cards_list):
+    for _ in range(cards[i + 1]):
+        for copy in range(i + 2, i + card + 2):
+            cards[copy] += 1
+    part2 += cards[i + 1]
+
+print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
+
+
